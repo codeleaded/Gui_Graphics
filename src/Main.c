fe950b419567703131b0684a3382277480db1b54
codeleaded <codeleaded@gmail.com>
2025-12-01 20:21:25 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -7,8 +7,11 @@ void Update(AlxWindow* w){
     
     Clear(BLACK);
     
-    Circle_R_Render(WINDOW_STD_ARGS,(Vec2){ 400.0f,200.0f },(Vec2){ 400.0f,200.0f },BLUE);
-    Circle_R_RenderWire(WINDOW_STD_ARGS,(Vec2){ 400.0f,200.0f },(Vec2){ 400.0f,200.0f },WHITE,1.0f);
+    //Circle_R_Render(WINDOW_STD_ARGS,(Vec2){ 400.0f,200.0f },(Vec2){ 400.0f,200.0f },BLUE);
+    //Circle_R_RenderWire(WINDOW_STD_ARGS,(Vec2){ 400.0f,200.0f },(Vec2){ 400.0f,200.0f },WHITE,1.0f);
+
+    Circle_C_Render(WINDOW_STD_ARGS,(Vec2){ 900.0f,400.0f },(Vec2){ 800.0f,200.0f },(Vec2){ 0.5f,0.5f },BLUE);
+    Circle_C_RenderWire(WINDOW_STD_ARGS,(Vec2){ 900.0f,400.0f },(Vec2){ 800.0f,200.0f },(Vec2){ 0.5f,0.5f },WHITE,1.0f);
 }
 void Delete(AlxWindow* w){
 
